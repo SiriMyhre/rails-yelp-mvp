d53b7a60c0a975f7d1044b2837457a3dc6f7816b
Siri Myhre <siri.o.myhre@gmail.com>
2018-08-01 17:01:48 +0100

    finished the show, new, and create actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+
+  root to: "restaurants#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :restaurants
+
+
+  # Read all - index
+  # Read one - show
+  # Create - new & create
+  # Update - edit & Update
+  # Delete -destoy
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,44 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number: '223 444 121',
+    category:     'belgian'
+  },
+  {
+    name:         'Bishoom',
+    address:      '7 Boundary St, Paris E2 7JE',
+    phone_number: '323 444 121',
+    category:     'french'
+  },
+  {
+    name:         'Kishoom',
+    address:      '7 Boundary St, Oslo E2 7JE',
+    phone_number: '423 444 121',
+    category:     'japanese'
+  },
+  {
+    name:         'Mishoom',
+    address:      '7 Boundary St, Copenhage E2 7JE',
+    phone_number: '523 444 121',
+    category:     'italian'
+  },
+  {
+    name:         'Vishoom',
+    address:      '7 Boundary St, Lisbon E2 7JE',
+    phone_number: '623 444 121',
+    category:     'chinese'
+  }
+
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
+
